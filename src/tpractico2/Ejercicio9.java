cb4a9cfecf33f58657d4c0b35d934f76a04f2635
Tevan88 <tevanrivarola@gmail.com>
2025-08-20 21:15:40 -0300

    Listo el 9

---
+++ src/tpractico2/Ejercicio9.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tpractico2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Esteban
+ */
+public class Ejercicio9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String zona;
+        double peso, precioProducto;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Ingrese el precio del producto: ");
+        precioProducto = Double.parseDouble(input.nextLine());
+        System.out.print("Ingrese el peso del paquete en kg: ");
+        peso = Double.parseDouble(input.nextLine());
+        System.out.print("Ingrese la zona de envio (Nacional/Internacional): ");
+        zona = input.nextLine();
+        
+        System.out.println("El costo de envio es: " + calcularCostoEnvio(peso, zona));
+        System.out.println("El total a pagar es: " + calcularTotalCompra(precioProducto, peso, zona));
+        
+        
+    }
+    
+    static double calcularCostoEnvio(double peso, String zona) {
+        double costoEnvio;
+        if (zona.equalsIgnoreCase("Nacional")) {
+        costoEnvio = peso * 5;
+    } else if (zona.equalsIgnoreCase("Internacional")) {
+        costoEnvio = peso * 10;
+    } else {
+        costoEnvio = 0; 
+        }
+        return costoEnvio;
+    } 
+    
+    
+    static double calcularTotalCompra(double precioProducto, double peso, String zona) {
+        double costoEnvioTotal = calcularCostoEnvio(peso, zona);
+        return precioProducto + costoEnvioTotal;
+    }
+}
